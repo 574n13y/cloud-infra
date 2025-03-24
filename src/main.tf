7fde304058e93041c5f3cee60e84ca2b78ed204d
VIVESH <35293085+574n13y@users.noreply.github.com>
2025-03-24 22:01:59 +0530

    Create main.tf

---
+++ src/main.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+  acl    = "private"
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_ec2_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name        = var.instance_name
+    Environment = var.environment
+  }
+}
