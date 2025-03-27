1aea2bf8215799e22265f1e4edbae1af249c23b4
Stanley <vivesht07@gmail.com>
2025-03-27 18:00:06 +0530

    backend.tf bucket added

--- src/backend.tf
+++ src/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "stanley-cloud-infra"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
+    bucket         = "stanley-cloud-infra-tfstate-$(date +%s)"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
     versioning {
       enabled = true
     }
