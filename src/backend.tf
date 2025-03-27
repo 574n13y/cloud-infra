5e9f3b7072ecb64acd04a6de0575efe3c33f50fa
Stanley <vivesht07@gmail.com>
2025-03-27 17:47:50 +0530

    features/fix-issues/playbook/secure

--- src/backend.tf
+++ src/backend.tf
@@ -1,3 +1,12 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "stanley-cloud-infra"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    versioning {
+      enabled = true
+    }
+    dynamodb_table = "terraform-locks"
+  }
 }
